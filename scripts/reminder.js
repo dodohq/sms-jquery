f300c0e950fa9025a24a5e71a051c17c9d46012f
pammelang <poppyfights@gmail.com>
2017-12-27 17:12:40 +0800

    Endpoints integrated. WIP delete timeslot and orders page

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,49 +1,53 @@
 
 const TIMESLOTS = [
-    "09:00", "10:00", "11:00", "12:00", "13:00", "14:00",
-    "15:00", "16:00", "17:00", "18:00", "19:00", "20:00",
+    "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00",
+    "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00",
 ]
 
 $(document).ready(function() { 
     TIMESLOTS.forEach((i) => {
-        $("#startTime").append(`
-        <option value='${i}'> ${i} </option>`);
-        $("#endTime").append(`
+        $("#timeSlots").append(`
         <option value='${i}'> ${i} </option>`);
     });
-    $("#serviceProvider").change(() => {
-        $("#timeSlots").css("display", "block");
-    });
-    $("form").append('<button type="submit">Submit</button>');
     $("form").submit((e) => {
         e.preventDefault();
-        if ($("#timeSlots").val()) {
-            handleSubmit();
-        } else {
-            alert("Make sure you've selected a timeslot!");
-        }    
+        handleSubmit();   
     });
 });
 
 function handleSubmit() {
-    var form = $("form")[0];
-    var result = {
-        start_time: $("#startTime").val(), 
-        end_time: $("#endTime").val(),
-        provider_id: $("input[name='serviceProvider']:checked").val(),
-    }
-    var data = new FormData(form);
-    var output = [];
-    for (const entry of data) {
-      output.push(entry[1]);
-    };
-    $.ajax({
-        url: "localhost:8080/api/time_slot", 
-        type: "POST", 
-        data: result, 
-        success: function(s) {
-            console.log(s);
-        }
-    })
-    // alert(`Customers will receive the delivery notification at ${output[1]} the day before delivery.`)
+    var provider_id = getUrlParam('provider_id');
+    console.log(provider_id);
+    var result = $.param({
+        reminder_time: $("#timeSlots").val(),
+    });
+    if (result) {
+        $.ajax({
+            url: `http://localhost:8080/api/provider/${provider_id}/set_reminder`, 
+            type: "PUT", 
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: result, 
+            crossDomain: true,
+        })
+        .done((data) => {
+            alert(`You have successfully set the reminder time to ${result.reminder_time}!`);
+        })
+        .fail((err) => {
+            console.log(err);
+        });
+    } else {
+        alert("Make sure you've selected a timeslot!");
+    }   
 };
+
+function getUrlParam(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "))
+}
